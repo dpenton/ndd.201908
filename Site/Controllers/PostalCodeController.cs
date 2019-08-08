368843af5cfc5a18853fe88f7b801852a66c4e96
David Penton <davidpenton-match@users.noreply.github.com>
2019-08-08 04:52:42 -0500

    .NET Core 2.2 code

---
+++ Site/Controllers/PostalCodeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Site.Data;
+
+namespace Site.Controllers
+{
+	[Route("api/postalcode/{pc}")]
+	[ApiController]
+	public class PostalCodeController : ControllerBase
+	{
+		private readonly IPostalCodeRepository _repository;
+
+		public PostalCodeController(IPostalCodeRepository repository)
+		{
+			_repository = repository;
+		}
+
+		[HttpGet]
+		public async Task<string> Get(string pc)
+		{
+			return await _repository.Get(pc);
+		}
+	}
+}
---
+++ Site/Data/PostalCodeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Site.Data
+{
+	public interface IPostalCodeRepository
+	{
+		Task<string> Get(string pc);
+	}
+
+	public class PostalCodeRepository : IPostalCodeRepository
+	{
+		private readonly HttpClient _client;
+
+		public PostalCodeRepository()
+		{
+			_client = new HttpClient {
+				BaseAddress = new Uri("http://api.zippopotam.us")
+			};
+		}
+
+		public async Task<string> Get(string pc)
+		{
+			var r = await _client.GetAsync($"/us/{pc}");
+
+			return await r.Content.ReadAsStringAsync();
+		}
+	}
+}
